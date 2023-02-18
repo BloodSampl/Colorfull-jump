c7eee57c864ec01e1168c46b9fa4e354b613b111
BloodSample <>
2023-02-18 11:42:11 +0400

    Spawn points done / Random color prefab respawn done

--- Collorfull jump/Assets/Scripts/Collectbles/CollectbleColor.cs
+++ Collorfull jump/Assets/Scripts/Collectbles/CollectbleColor.cs
@@ -7,11 +7,17 @@ public class CollectbleColor : MonoBehaviour
     [SerializeField] GamePlayColors color;
     [SerializeField] ColorManager colorManager;
     SpriteRenderer renderer;
-    public GamePlayColors Color => color;
+    public GamePlayColors Color => color; // to access it in the collision
 
+    private void Awake()
+    {
+        colorManager = FindObjectOfType<ColorManager>();
+    }
     private void Start()
     {
-        renderer= gameObject.GetComponent<SpriteRenderer>();
-        renderer.color = colorManager.GetColor(color);
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.color = colorManager.GetColor(GetPrefabRandomColor());
     }
+    private GamePlayColors GetPrefabRandomColor()
+    { return color = colorManager.GetRandomColor(); }
 }
