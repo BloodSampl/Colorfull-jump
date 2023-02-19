92f9268e1fdee7620bf80be84db64e2bf692aed2
BloodSample <>
2023-02-19 09:44:07 +0400

    Scene management, wining/Losing and Level 2 Done

--- Collorfull jump/Assets/Scripts/Player/PlayerColorCollisionController.cs
+++ Collorfull jump/Assets/Scripts/Player/PlayerColorCollisionController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerColorCollisionController : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class PlayerColorCollisionController : MonoBehaviour
             }
             else
             {
-                Debug.Log("Wrong Color");
+                SceneManager.LoadScene("GameOver");              
             }
         }
     }
